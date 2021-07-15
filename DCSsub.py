9e943a10c9aefa7252c3bf0c7bcd62cc73bf24ef
Thomas Eder <thomas.eder@vetmeduni.ac.at>
2021-07-15 11:57:48 +0200

    Update DCSsub.py

--- DCSsub.py
+++ DCSsub.py
@@ -4,7 +4,7 @@
 """
 %prog <BAM> <BED> <TXT> <INT> <BAM,...> [options]
 
-Based on the input <BAM> file DE-peaks will be simulated in the provided <BED> regions, <TXT> chromosmes to be used and their length tab separated, 
+Based on the input <BAM> file DE-peaks will be sub-sampled in the provided <BED> regions, <TXT> chromosmes to be used and their length tab separated, 
 <INT> defines the number of simulated replicates of the two samples,
 the input/control Bam files <BAM,...> comma-separated, will be used to create an input/control Bam file for the simulation.
 @author: Thomas Eder
